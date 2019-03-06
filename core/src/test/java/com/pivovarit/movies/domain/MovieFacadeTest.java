1c31fb1f666bcdf76a64b388fe536013f285ba08
pivovarit <gpiwowarek@gmail.com>
2019-03-06 15:24:11 +0100

    Revert to junit4

--- core/src/test/java/com/pivovarit/movies/domain/MovieFacadeTest.java
+++ core/src/test/java/com/pivovarit/movies/domain/MovieFacadeTest.java
@@ -2,22 +2,22 @@ package com.pivovarit.movies.domain;
 
 import com.pivovarit.movies.dto.MovieDto;
 import com.pivovarit.movies.dto.MovieTypeDto;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MovieFacadeTest {
+public class MovieFacadeTest {
 
     private MovieFacade movieFacade;
 
-    @BeforeEach
-    void setup() {
+    @Before
+    public void setup() {
         movieFacade = MovieFacade.inMemoryMovieFacade();
     }
 
     @Test
-    void shouldAddMovie() {
+    public void shouldAddMovie() {
         // given
         String title = "foo";
         MovieDto dto = new MovieDto(42l, title, new MovieTypeDto("NEW"));
@@ -29,34 +29,4 @@ class MovieFacadeTest {
         MovieDto result = movieFacade.findByTitle(title);
         assertThat(result).isEqualTo(dto);
     }
-
-    @Test
-    void shouldAddMovie2() {
-        // given
-        String title = "foo";
-        MovieDto dto = new MovieDto(42l, title, new MovieTypeDto("NEW"));
-
-        // when
-        movieFacade.add(dto);
-
-        // then
-        MovieDto result = movieFacade.findByTitle(title);
-        assertThat(result).isEqualTo(dto);
-    }
-
-    @Test
-    void shouldAddMovie3() {
-        // given
-        String title = "foo";
-        MovieDto dto = new MovieDto(42l, title, new MovieTypeDto("NEW"));
-
-        // when
-        movieFacade.add(dto);
-
-        // then
-        MovieDto result = movieFacade.findByTitle(title);
-        assertThat(result).isEqualTo(dto);
-    }
-
-
 }
