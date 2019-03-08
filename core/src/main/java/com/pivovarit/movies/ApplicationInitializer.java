b75c2951187479ca56af7dfcdbbed5384fda364a
pivovarit <gpiwowarek@gmail.com>
2019-03-08 09:34:15 +0100

    Add more movies

--- core/src/main/java/com/pivovarit/movies/ApplicationInitializer.java
+++ core/src/main/java/com/pivovarit/movies/ApplicationInitializer.java
@@ -9,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintViolationException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -21,12 +23,27 @@ class ApplicationInitializer implements CommandLineRunner {
     public void run(String... args) {
         System.out.println("Started!");
 
-        try {
-            movieFacade.add(new MovieDto("spdrmun1", "Spiderman1", new MovieTypeDto("NEW"), 2012));
-        } catch (Exception e) {
-            log.warn("duplicates ignored", e);
-        }
-        movieFacade.priceFor(new MovieTypeDto("NEW"))
-            .ifPresent(System.out::println);
+        List<MovieDto> movies = Arrays.asList(
+            new MovieDto("spdrmun1", "Spiderman1", new MovieTypeDto("OLD"), 2012),
+            new MovieDto("spdrmun2", "Spiderman2", new MovieTypeDto("OLD"), 2013),
+            new MovieDto("incptn", "Inception", new MovieTypeDto("REGULAR"), 2014),
+            new MovieDto("plpfct1", "Pulp Fiction", new MovieTypeDto("OLD"), 1994),
+            new MovieDto("grnbook", "Grenbook", new MovieTypeDto("NEW"), 2019),
+            new MovieDto("mtrx", "Matrix", new MovieTypeDto("OLD"), 1997),
+            new MovieDto("cztrpncrn", "Czterej Pancerni", new MovieTypeDto("OLD"), 1966)
+        );
+
+        movies.forEach(m -> {
+            try {
+                movieFacade.add(m);
+            } catch (Exception e) {
+                log.warn("duplicates ignored", e);
+
+            }
+        });
+
+        movieFacade.priceFor(new MovieTypeDto("NEW")).ifPresent(price -> System.out.println("NEW price: " + price));
+        movieFacade.priceFor(new MovieTypeDto("OLD")).ifPresent(price -> System.out.println("OLD price: " + price));
+        movieFacade.priceFor(new MovieTypeDto("REGULAR")).ifPresent(price -> System.out.println("REGULAR price: " + price));
     }
 }
