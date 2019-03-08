e5f4853929cc579ff65a582ec6773ced6f7d069c
pivovarit <gpiwowarek@gmail.com>
2019-03-08 15:44:56 +0100

    Add email

--- core/src/main/java/com/pivovarit/movies/Application.java
+++ core/src/main/java/com/pivovarit/movies/Application.java
@@ -3,6 +3,10 @@ package com.pivovarit.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+// grzegorz.piwowarek@bottega.com.pl
+
 @SpringBootApplication
 class Application {
     public static void main(String[] args) {
